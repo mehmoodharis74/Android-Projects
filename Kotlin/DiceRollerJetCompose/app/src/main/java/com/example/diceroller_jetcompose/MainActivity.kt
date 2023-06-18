5216b5ddbe85a69109bb00dbdf68ca4b58338fbd
mehmo <mehmoodharis74@gmail.com>
2023-06-18 20:33:40 +0500

    Add Dice Roller App in JetPackCompose and Kotlin

--- Kotlin/DiceRollerJetCompose/app/src/main/java/com/example/diceroller_jetcompose/MainActivity.kt
+++ Kotlin/DiceRollerJetCompose/app/src/main/java/com/example/diceroller_jetcompose/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.example.diceroller_jetcompose
 
+
+import android.media.Image
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.sp
 import com.example.diceroller_jetcompose.ui.theme.DiceRollerJetComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +33,44 @@ class MainActivity : ComponentActivity() {
         setContent {
             DiceRollerJetComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                DiceRoller()
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DiceRoller(modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)) {
+    var result by remember { mutableStateOf( 1) }
+    val imageResource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = "1"
+        )
+        Spacer(modifier = Modifier.size(16.dp))
+        Button(onClick = { result = (1..6).random() },) {
+            Text(stringResource(id = R.string.roll), fontSize = 24.sp)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     DiceRollerJetComposeTheme {
-        Greeting("Android")
+        DiceRoller()
     }
 }
