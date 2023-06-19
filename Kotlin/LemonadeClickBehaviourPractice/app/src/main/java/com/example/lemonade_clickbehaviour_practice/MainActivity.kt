4db8f6cb3994b8b517a52e462c59b64f1beab940
mehmo <mehmoodharis74@gmail.com>
2023-06-19 12:11:31 +0500

    Revert "Added Lemonade Project for practicing click behaviour"
    This reverts commit 7d76df834ec6b865e5831b8bf4cb944b0a26c537.

--- Kotlin/LemonadeClickBehaviourPractice/app/src/main/java/com/example/lemonade_clickbehaviour_practice/MainActivity.kt
+++ Kotlin/LemonadeClickBehaviourPractice/app/src/main/java/com/example/lemonade_clickbehaviour_practice/MainActivity.kt
@@ -18,21 +18,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             LemonadeClickBehaviourPracticeTheme {
                 // A surface container using the 'background' color from the theme
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
             }
         }
     }
 }
 
 @Composable
-fun Lemonade(name: String) {
-
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LemonadeClickBehaviourPracticeTheme {
-
+        Greeting("Android")
     }
 }
