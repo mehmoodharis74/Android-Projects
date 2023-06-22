cd5682c68ce618391c3dd556a3e3c39ade67dcbf
mehmo <mehmoodharis74@gmail.com>
2023-06-22 22:53:42 +0500

    fix some font size issues and other

--- Kotlin/LoginPage/app/src/main/java/com/example/loginpage/MainActivity.kt
+++ Kotlin/LoginPage/app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -62,7 +62,7 @@ fun CardFunction(){
             .padding(bottom = 10.dp))
     {
         Box(modifier = Modifier
-            .padding(start = 5.dp, top = 12.dp, bottom = 12.dp, end = 5.dp)){
+            .padding(start = 5.dp, top = 14.dp, bottom = 14.dp, end = 5.dp)){
             Row {
                 Icon(Icons.Filled.Send, contentDescription = "Google",
                 modifier = Modifier
