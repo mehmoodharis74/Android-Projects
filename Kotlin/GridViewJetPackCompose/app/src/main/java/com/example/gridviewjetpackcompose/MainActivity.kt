6e4ad90f02fa27e005e114871ddc295ed8157edc
mehmo <mehmoodharis74@gmail.com>
2023-06-23 22:12:41 +0500

    Some work in grid view task

--- Kotlin/GridViewJetPackCompose/app/src/main/java/com/example/gridviewjetpackcompose/MainActivity.kt
+++ Kotlin/GridViewJetPackCompose/app/src/main/java/com/example/gridviewjetpackcompose/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.gridviewjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.gridviewjetpackcompose.model.Topic
 import com.example.gridviewjetpackcompose.ui.theme.GridViewJetPackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,8 +31,19 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CardLayout(modifier: Modifier=Modifier){
+fun CardLayout(topic:Topic, modifier: Modifier=Modifier){
+    Card(modifier = modifier) {
+        Row(modifier = Modifier
+            .height(68.dp)
+            .fillMaxWidth()) {
+            Image(painter = painterResource(id = topic.imageId)
+                , contentDescription = stringResource(id = topic.text), modifier = Modifier
+                    .fillMaxHeight()
+                    .width(68.dp))
+        }
+    }
 
 }
 @Composable
@@ -41,6 +55,6 @@ fun mainFunction() {
 @Composable
 fun DefaultPreview() {
     GridViewJetPackComposeTheme {
-        CardLayout()
+        //CardLayout()
     }
 }
