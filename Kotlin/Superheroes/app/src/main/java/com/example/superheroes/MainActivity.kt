606d7bfd6091fce43ae87ae2faaba8c158789e7c
mehmo <mehmoodharis74@gmail.com>
2023-06-26 14:26:13 +0500

    Some work in SuperHeroes

--- Kotlin/Superheroes/app/src/main/java/com/example/superheroes/MainActivity.kt
+++ Kotlin/Superheroes/app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.superheroes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.superheroes.model.data
+import com.example.superheroes.model.model
 import com.example.superheroes.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +37,40 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HeroItem(hero:data){
+    Card(modifier = Modifier
+        .height(72.dp)
+        .padding(16.dp)
+        .clip(RoundedCornerShape(16.dp))
+        ,elevation = CardDefaults.cardElevation(defaultElevation = 2.dp) ) {
 
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            HeroDescription(heroName = He, heroDes = , modifier = )
+
+        }
+        
+    }
+}
+@Composable
+fun HeroIcon(@DrawableRes imageId:Int){
+    Box {
+       Image(painter = painterResource(id = imageId), contentDescription = "Image of the superhero",
+       modifier = Modifier
+           .size(72.dp)
+           .clip(RoundedCornerShape(8.dp)))
+    }
+}
+@Composable
+fun HeroDescription(@StringRes heroName:Int, @StringRes heroDes:Int ,modifier: Modifier){
+    Column(modifier = modifier) {
+        Row {
+           Text(stringResource(id = heroName), style = MaterialTheme.typography.displaySmall)
+            Text(stringResource(id = heroDes), style = MaterialTheme.typography.bodyLarge )
+        }
+    }
+}
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
--- Kotlin/Superheroes/app/src/main/java/com/example/superheroes/model/data.kt
+++ Kotlin/Superheroes/app/src/main/java/com/example/superheroes/model/data.kt
@@ -3,36 +3,36 @@ package com.example.superheroes.model
 import com.example.superheroes.R
 
 object data {
-    val Heroes = listOf<model>(
+    val Heroes = listOf(
         model(
             nameRes = R.string.hero1,
             descriptionRes = R.string.description1,
-            imageRes = R.drawable.android_superhero1
+            imageRes = R.drawable.a1
         ),
         model(
             nameRes = R.string.hero2,
             descriptionRes = R.string.description2,
-            imageRes = R.drawable.android_superhero2
+            imageRes = R.drawable.a2
         ),
         model(
             nameRes = R.string.hero3,
             descriptionRes = R.string.description3,
-            imageRes = R.drawable.android_superhero3
+            imageRes = R.drawable.a3
         ),
         model(
             nameRes = R.string.hero4,
             descriptionRes = R.string.description4,
-            imageRes = R.drawable.android_superhero4
+            imageRes = R.drawable.a4
         ),
         model(
             nameRes = R.string.hero5,
             descriptionRes = R.string.description5,
-            imageRes = R.drawable.android_superhero5
+            imageRes = R.drawable.a5
         ),
         model(
             nameRes = R.string.hero6,
             descriptionRes = R.string.description6,
-            imageRes = R.drawable.android_superhero6
+            imageRes = R.drawable.a6
         )
     )
 }
