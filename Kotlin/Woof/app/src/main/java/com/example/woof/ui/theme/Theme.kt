97b60ef80edfafa3ac0704bce3b7727abf43d91b
mehmo <mehmoodharis74@gmail.com>
2023-06-28 22:46:22 +0500

    Some work in SuperHeroes

--- Kotlin/Woof/app/src/main/java/com/example/woof/ui/theme/Theme.kt
+++ Kotlin/Woof/app/src/main/java/com/example/woof/ui/theme/Theme.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -92,8 +93,14 @@ fun WoofTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            val activity  = view.context as Activity
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                activity.window.navigationBarColor = colorScheme.primary.copy(alpha = 0.08f).compositeOver(colorScheme.surface.copy()).toArgb()
+                activity.window.statusBarColor = colorScheme.background.toArgb()
+                ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = !darkTheme
+
+
+            }
         }
     }
 
