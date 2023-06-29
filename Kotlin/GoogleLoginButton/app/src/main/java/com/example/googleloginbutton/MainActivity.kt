281f50956f1d17f99d8180f3c3b2cb18aa0fd440
mehmo <mehmoodharis74@gmail.com>
2023-06-29 20:52:02 +0500

    Added Google Sign UP Button Task

--- Kotlin/GoogleLoginButton/app/src/main/java/com/example/googleloginbutton/MainActivity.kt
+++ Kotlin/GoogleLoginButton/app/src/main/java/com/example/googleloginbutton/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.googleloginbutton
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
@@ -35,11 +35,13 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun GoogleSignUpButton() {
+    var clicked by remember { mutableStateOf(false) }
     Surface(modifier = Modifier
         .border(1.dp, Color.Gray)
-        .width(200.dp)
-        .height(36.dp),
-        shape = MaterialTheme.shapes.medium) {
+        .height(36.dp)
+        .clickable(onClick = { clicked = !clicked })
+        .animateContentSize(animationSpec = tween(durationMillis = 5, delayMillis = 1))
+        , shape = MaterialTheme.shapes.medium) {
 
         Row(verticalAlignment = Alignment.CenterVertically) {
 
@@ -49,8 +51,18 @@ fun GoogleSignUpButton() {
                 .padding(start = 12.dp)
                 .size(18.dp))
             Spacer(modifier = Modifier.width(8.dp))
-            Text(text = "Sign Up with Google", modifier = Modifier.padding(end = 16.dp),
+            Text(text = "Sign Up with Google",
                 style = MaterialTheme.typography.bodySmall)
+            if(clicked){
+                CircularProgressIndicator(modifier = Modifier
+                    .padding(start = 12.dp).size(16.dp)
+                    , color = MaterialTheme.colorScheme.primary, strokeWidth = 2.dp,
+                )
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+
+
+
         }
 
 
