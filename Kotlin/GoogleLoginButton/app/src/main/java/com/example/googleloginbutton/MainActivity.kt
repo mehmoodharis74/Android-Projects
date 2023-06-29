5b5d7fef7ccd4c84b38bc445b7dda373a88b6dc6
mehmo <mehmoodharis74@gmail.com>
2023-06-29 20:32:54 +0500

    Added Google Sign UP Button Task

--- Kotlin/GoogleLoginButton/app/src/main/java/com/example/googleloginbutton/MainActivity.kt
+++ Kotlin/GoogleLoginButton/app/src/main/java/com/example/googleloginbutton/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.googleloginbutton
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.googleloginbutton.ui.theme.GoogleLoginButtonTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +35,26 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun GoogleSignUpButton() {
+    Surface(modifier = Modifier
+        .border(1.dp, Color.Gray)
+        .width(200.dp)
+        .height(36.dp),
+        shape = MaterialTheme.shapes.medium) {
 
+        Row(verticalAlignment = Alignment.CenterVertically) {
+
+            Icon(painter = painterResource(id = R.drawable.google)
+                , contentDescription ="Google Icon", tint = Color.Unspecified ,
+            modifier = Modifier
+                .padding(start = 12.dp)
+                .size(18.dp))
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "Sign Up with Google", modifier = Modifier.padding(end = 16.dp),
+                style = MaterialTheme.typography.bodySmall)
+        }
+
+
+    }
 }
 
 
