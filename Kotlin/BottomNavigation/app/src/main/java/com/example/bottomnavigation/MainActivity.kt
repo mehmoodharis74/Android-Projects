924fb4e2a4bc70df08d8c911733def3645850e4c
mehmo <mehmoodharis74@gmail.com>
2023-07-14 01:23:22 +0500

    Bottom Navigation Task Added

--- Kotlin/BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ Kotlin/BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -19,6 +22,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.bottomnavigation.NavGraph.Home
+import com.example.bottomnavigation.NavGraph.Screens
 import com.example.bottomnavigation.NavGraph.Settings
 import com.example.bottomnavigation.ui.theme.BottomNavigationTheme
 
@@ -29,20 +33,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             BottomNavigationTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navigator = rememberNavController()
-                    NavHost(navController = navigator, startDestination = Home.route ){
-                        composable(Home.route){ HomeScreen(navigator)}
-                        composable(Settings.route){ SettingScreen(navigator)}
+                Scaffold(bottomBar = {}){
+                    val bottomList = listOf(Home,Settings)
+
+                    var selectedIndex = rememberSaveable{ mutableStateOf(-1) }
+
+
+
+                    Box(modifier = Modifier.padding(it)){
+                        val navigator = rememberNavController()
+                        NavHost(navController = navigator, startDestination = Home.route ){
+                            composable(Home.route){ HomeScreen(navigator)}
+                            composable(Settings.route){ SettingScreen(navigator)}
+                        }
                     }
-                    //create bottom bar using scafold
-
-
-
-
                 }
             }
         }
--- Kotlin/BottomNavigation/app/src/main/java/com/example/bottomnavigation/NavGraph/Screens.kt
+++ Kotlin/BottomNavigation/app/src/main/java/com/example/bottomnavigation/NavGraph/Screens.kt
@@ -1,13 +1,24 @@
 package com.example.bottomnavigation.NavGraph
 
-interface Screens {
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+interface Screens {
     val route:String
+    val title:String
+    val icon: ImageVector
 
 }
 object Home:Screens{
     override val route: String = "home"
+    override val icon: ImageVector = Icons.Filled.Home
+    override val title: String = "Home"
 }
 object Settings:Screens{
     override val route: String = "settings"
+    override val icon: ImageVector = Icons.Filled.Settings
+    override val title: String = "Settings"
 }
