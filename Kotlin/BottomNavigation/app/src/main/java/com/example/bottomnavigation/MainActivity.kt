0308654acaf809b2a9035a9358a734a5233e5722
mehmo <mehmoodharis74@gmail.com>
2023-07-18 16:20:02 +0500

    Completed Bottom Navigation Task

--- Kotlin/BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ Kotlin/BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -109,7 +109,10 @@ fun BottomPreview() {
 
             Box(modifier = Modifier.padding(it)){
 
-
+                NavHost(navController = navigator, startDestination = Home.route ){
+                    composable(Home.route){ HomeScreen(navigator)}
+                    composable(Settings.route){ SettingScreen()}
+                }
             }
         }
     }
