f8187313be085226d7dca0fc30b1bb73ef0dea3f
mehmo <mehmoodharis74@gmail.com>
2023-07-24 11:29:11 +0500

    Added Little Lemon App

--- Kotlin/Little Lemon App/app/src/main/java/com/example/littlelemon/DishDetails.kt
+++ Kotlin/Little Lemon App/app/src/main/java/com/example/littlelemon/DishDetails.kt
@@ -29,7 +29,7 @@ fun DishDetails(id: Int) {
         Column(verticalArrangement = Arrangement.spacedBy(10.dp),
         modifier = Modifier.padding(horizontal = 10.dp)) {
             Text(text =  dish.name, style = MaterialTheme.typography.h6)
-            Text(text =dish.description, style = MaterialTheme.typography.body1)
+
             Counter()
             Button(onClick = { /*TODO*/ }) {
                 Text(textAlign = TextAlign.Center,text = stringResource(id = R.string.add_for) + " $${dish.price}")
