742ce8acafe9493565f2d9ad620d21d04f96e301
mehmo <mehmoodharis74@gmail.com>
2023-07-29 20:26:36 +0500

    Added Tech Hub Store App

---
+++ Kotlin/techhub/app/src/main/java/com/example/techhub/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.techhub.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val light_primary = Color(0xFF000000)
+val dark_primary = Color(0xFFFFFFFF)
+val on_Light_primary = Color(0xFFFFFFFF)
+val on_Dark_primary = Color(0xFF000000)
+
+val heading_Light = Color(0xFF000000)
+val heading_Dark = Color(0xFFFFFFFF)
+val text_Light = Color(0xFF3A3A3A)
+val text_Dark = Color(0xFFE0E0E0)
+
+val background_Light = Color(0xFFFFFFFF)
+val background_Dark = Color(0xFF000000)
+
+val onBackground_Light = Color(0xFF000000)
+val onBackground_Dark = Color(0xFFFFFFFF)
